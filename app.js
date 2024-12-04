15cc1ab79d8794b3b1913d4ca1e1061bbd6bb044
Aires Matos <aires18matos2@gmail.com>
2024-12-04 13:31:01 +0100

    exportando a configuração da banco de dados mariadb

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import homeRoutes from './src/routes/homeRoutes';
-
+import dotenv from 'dotenv';
+dotenv.config();
 import express  from 'express'
+
 
 class App {
   constructor(){
---
+++ src/config/database.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+module.exports = {
+  dialect: 'mariadb',
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE,
+  //define
+  //usar os campos de registros criando na tabela
+  //para saber quando m registro foi criado na tabela e quando um registro foi atualizado na tabela
+  define: {
+    timestamps: true,
+    underdcored: true,
+    undefinedAll: true,
+    'createdAt': 'creates_at',
+    'updatedAt': 'updated_at'
+  },
+  dialectOptions: {
+    timezone: 'África_Ocidental/Luanda',
+  },
+  timezone: 'África_Ocidental/Luanda'
+}
