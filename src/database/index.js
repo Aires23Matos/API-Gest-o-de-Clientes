4d75dedfc3e0cc327609c47bc29d7948d0f34ff7
Aires Matos <aires18matos2@gmail.com>
2024-12-10 18:29:43 +0100

    criando router responsavelRoutes

--- src/database/index.js
+++ src/database/index.js
@@ -5,8 +5,9 @@ import User from "../models/User";
 import Morada from "../models/Morada";
 import Contatos from '../models/Contatos';
 import DadosLicenca from "../models/DadosLicenca";
+import Responsavel from "../models/Responsavel";
 
-const models = [Cliente, User, Morada, Contatos, DadosLicenca];
+const models = [Cliente, User, Morada, Contatos, DadosLicenca, Responsavel];
 
 const connection = new Sequelize(databaseConfig);
 
