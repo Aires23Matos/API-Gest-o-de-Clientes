be885451f14bd45a75b35d22d363de76e309065a
Aires Matos <aires18matos2@gmail.com>
2024-12-11 16:55:15 +0100

    criando post/show/update no contatosController

--- src/controllers/ContatosController.js
+++ src/controllers/ContatosController.js
@@ -3,17 +3,59 @@ import Contatos from "../models/Contatos";
 class ContatosController{
   async store(req, res){
    try{
-    const novoContatos = await Contatos.create({
-      telefone: 999999999,
-      email: "ddddf@gmail.com",
-      web_site: "ww.com"
-    });
+    const novoContatos = await Contatos.create(req.body);
     res.json(novoContatos);
   }catch(e){
     console.log(e);
     res.status(400).json({
       errors: e.errors.map(err => err.message),
     });
+  }
+}
+
+//Index
+async index(req, res){
+  try{
+    const contatos = await Contatos.findAll();
+    return res.json(contatos);
+  }catch(e){
+    return res.json(null);
+  }
+}
+
+//Show
+async Show(req,res){
+  try{
+    const {id} = req.params
+    const contato = await Contatos.findByPk(id);
+    return res.json(contato);
+  }catch(e){
+    return res.json(null);
+  }
+}
+
+//update
+async update(req,res){
+  try{
+    const {id} = req.params;
+    if(!id){
+      return res.status(400).json({
+        errors: ['ID não encontrado'],
+      });
+    }
+    const contatos = await Contatos.findByPk(id);
+    if(!contatos){
+      return res.status(400).json({
+        errors: ['Contato não existe'],
+      })
+    }
+
+    const novoContatos = contatos.update(req.body);
+    return res.json(novoContatos);
+  }catch(e){
+    return res.status(400).json({
+      errors: e.errors.map(err => err.message),
+    })
   }
 }
 }
--- src/routes/contatosRoutes.js
+++ src/routes/contatosRoutes.js
@@ -5,5 +5,7 @@ import ContatosController from "../controllers/ContatosController";
 const router = new Router();
 
 router.post('/', ContatosController.store);
-
+router.get('/', ContatosController.index);
+router.get('/:id', ContatosController.Show);
+router.put('/:id', ContatosController.update);
 export default router;
