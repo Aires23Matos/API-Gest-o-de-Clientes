8a451830ccb51c6fb2930b51b6ccd9cc2659c87c
Aires Matos <aires18matos2@gmail.com>
2024-12-11 17:59:40 +0100

    adicionando get/put no ResponsavelController

--- src/controllers/ReponsavelController.js
+++ src/controllers/ReponsavelController.js
@@ -3,11 +3,7 @@ import Responsavel from "../models/Responsavel";
 class ResponsavelController{
   async store(req, res){
     try{
-      const novoResponsavel = await Responsavel.create({
-        nome: 'Aires',
-        telefone: 999999999,
-        email: 'aires@gmail.com',
-      });
+      const novoResponsavel = await Responsavel.create(req.body);
       res.json(novoResponsavel);
     }catch(e){
       console.log(e);
@@ -16,6 +12,53 @@ class ResponsavelController{
       });
     }
   };
+
+  //Index
+  async index(req, res){
+    try{
+      const responsavel = await Responsavel.findAll();
+      return res.json(responsavel);
+    }catch(e){
+      return res.json(null);
+    }
+  }
+
+  //Show
+  async show(req, res){
+    try{
+      const {id} = req.params;
+      const responsaveis = await Responsavel.findByPk(id);
+      return res.json(responsaveis);
+    }catch(e){
+      return res.json(null);
+    }
+  }
+
+  //Update
+  async update(req, res){
+    try{
+      const {id} =req.params;
+      if(!id){
+        return res.status(400).json({
+          errors: ['ID não enviado'],
+        });
+      }
+
+      const responsavel = await Responsavel.findByPk(id);
+      if(!responsavel){
+        return res.status(400).json({
+          errors: ['A tabela responsavel não existe'],
+        });
+      }
+
+      const novoResponsavel = responsavel.update(req.body);
+      return res.json(novoResponsavel);
+    }catch(e){
+      return res.status(400).json({
+        errors: e.errors.map(err => err.message),
+      });
+    }
+  }
 };
 
 export default  new ResponsavelController();
--- src/routes/responsavelRoutes.js
+++ src/routes/responsavelRoutes.js
@@ -4,6 +4,9 @@ import responsavelController from "../controllers/ReponsavelController";
 const router = new Router();
 
 router.post('/', responsavelController.store);
+router.get('/', responsavelController.index);
+router.get('/:id', responsavelController.show);
+router.put('/:id', responsavelController.update);
 
 export default router;
 
