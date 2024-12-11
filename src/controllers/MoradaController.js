33f9bff53fea553676ef14d2f41e4f17f28538fe
Aires Matos <aires18matos2@gmail.com>
2024-12-11 17:38:18 +0100

    adicionando get/put/ no MoradaController

--- src/controllers/MoradaController.js
+++ src/controllers/MoradaController.js
@@ -9,8 +9,52 @@ class MoradaController{
     res.status(400).json({
       errors: e.errors.map(err => err.message),
     });
+    }
   }
-}
+
+  //Index
+  async Index(req, res){
+    try{
+      const moradas = await Morada.findAll();
+      return res.json(moradas);
+    }catch(e){
+      return res.json(null);
+    }
+  }
+  //Show
+  async show(req, res){
+    try{
+      const {id} = req.params;
+      const morada = await Morada.findByPk(id);
+      return res.json(morada);
+    }catch(e){
+      return res.json(null);
+    }
+  }
+  //Update
+  async update(req, res){
+    try{
+      const {id} = req.params;
+      if(!id){
+        return res.status(400).json({
+          errors: ['ID não enviado.'],
+        });
+      }
+      const morada = await Morada.findByPk(id);
+      if(!morada){
+        return res.status(400).json({
+          errors: ['Morada não existe'],
+        })
+      }
+
+      const novaMorada = morada.update(req.body);
+      return res.json(novaMorada);
+    }catch(e){
+      return res.status(400).json({
+        errors: e.errors.map(err => err.message),
+      })
+    }
+  }
 }
 
 export default new MoradaController();
--- src/routes/moradaRoutes.js
+++ src/routes/moradaRoutes.js
@@ -4,5 +4,8 @@ import MoradaController from '../controllers/MoradaController';
 const router = new Router();
 
 router.post('/', MoradaController.store);
+router.get('/', MoradaController.Index);
+router.get('/:id', MoradaController.show);
+router.put('/:id', MoradaController.update);
 
 export default router;
